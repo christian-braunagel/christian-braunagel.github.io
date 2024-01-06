8c651f41be7d819f5cfb281269765645b5597766
ChristianBraunagel <62561756+ChristianBraunagel@users.noreply.github.com>
2024-01-06 14:03:12 +0100

    update footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,31 +3,27 @@ const Footer = () => {
     <footer id="footer">
       <div class="container">
         <div class="social-links">
-          <a href="https://twitter.com/bhushanborole26" class="twitter">
-            <i class="bx bxl-twitter"></i>
-          </a>
-          <a
-            href="https://www.instagram.com/bhushan_borole_/"
-            class="instagram"
-          >
-            <i class="bx bxl-instagram"></i>
-          </a>
-          <a href="https://github.com/bhushan-borole" class="google-plus">
+          <a href="https://github.com/christian-braunagel" class="google-plus">
             <i class="bx bxl-github"></i>
           </a>
           <a
-            href="https://www.linkedin.com/in/bhushan-borole/"
+            href="https://www.linkedin.com/in/christian-braunagel-6242b7228"
             class="linkedin"
           >
             <i class="bx bxl-linkedin"></i>
           </a>
-          <a href="mailto:borolebhushan8@gmail.com">
+          <a href="mailto:braunagel.christian+githubpages@gmail.com">
             <i class="bx bx-envelope"></i>
           </a>
         </div>
         <div class="credits" style={{ color: "#74808a" }}>
-          Made with <span style={{ color: "#e25555" }}>&hearts;</span> using
-          React
+          This page is based on the following projects developed by:<br/>
+          <a
+            href="https://github.com/bhushan-borole/bhushan-borole.github.io"
+          >
+          bhushan-borole
+          </a>
+          <br/>Cudos to these great developers.
         </div>
       </div>
     </footer>
